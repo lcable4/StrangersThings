36930dd5f88fd6945b62f41a39db8a17a51f3b26
UpcomingWhale <javascriptcodingguru@gmail.com>
2023-02-21 14:04:20 -0500

    added dynamic login/logout depending on token state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 
+function onLogoutClick()
+{
+  localStorage.removeItem("token");
+}
+
+const Login = () =>
+{
+return(
+  <Link to="/login">
+    <button>Login</button>
+  </Link>
+)
+}
+const Logout = ()=>
+{return(
+  <button onClick={onLogoutClick()}>Logout</button>
+)
+}
+
 const Navbar = () => {
   return (
     <div id="navbar">
       <h2> Stranger's Things</h2>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+      {
+        localStorage.getItem("token") ? <Logout/>:<Login/>
+      }
       <Link to="/registration">
         <button>Register</button>
       </Link>
