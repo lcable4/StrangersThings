bcb1656bf07d1e399bdde7c488dfc94c3a5d124b
Logan <logancable@ymail.com>
2023-02-22 13:38:34 -0800

    testing messages

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,10 +3,12 @@ import {Link, useParams} from "react-router-dom"
 import { getAllPosts, messages } from '../apiAdapters';
 
 
+
 export default function Details() {
   
     const { postId } = useParams();
     const [post, setPost] = useState(null);
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         async function fetchPost() {
@@ -21,45 +23,52 @@ export default function Details() {
         fetchPost();
     }, [postId]);
 
-    function sendMessage(postId, content)
+    function sendMessage(postId, content) {
+        
 
+        
+        messages(postId, content)
+        
+    }
     return (
     <>
-    <div>
-        <h1>Post Details</h1>
-        {
-            post ? (
-                <>
-                <h3>{post.title}</h3>
-                <p>{post.description}</p>
-                <p>{post.price}</p>
-                <p>{post.location}</p>
-                </>
-            ) : (
-                <p>loading</p>
-            )
-        }
-    </div>
-    <form onSubmit={(e) => {
-            e.preventDefault();
-            sendMessage(newUserName, newUserPass);
-        }}>
-            <p>
-                <label>
-                    Send a message:
-                    <input
-                        name="name"
-                        type="text"
-                        value={newUserName}
-                        required
-                        minLength="5"
-                        onChange={(e) => {
-                            setNewUserName(e.target.value);
-                        }}
-                    />
-                </label>    
-            </p>
+        <div>
+            <h1>Post Details</h1>
+            {
+                post ? (
+                    <>
+                    <h3>{post.title}</h3>
+                    <p>{post.description}</p>
+                    <p>{post.price}</p>
+                    <p>{post.location}</p>
+                    </>
+                ) : (
+                    <p>loading</p>
+                )
+            }
+        </div>
+        <form onSubmit={(e) => {
+                e.preventDefault();
+                sendMessage(postId, message);
+            }}>
+                
+                    <label>
+                        Send a message:
+                        <input
+                            name="message"
+                            type="text"
+                            value={message}
+                            required
+                            minLength="5"
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                        />
+                    </label>  
+                    <button type="submit">send message</button>  
+                
+            </form>
     <Link to="/">Go Back</Link>
     </>
-  )
+    )
 }
