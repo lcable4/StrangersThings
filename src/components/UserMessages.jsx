25393ef31720ee5b08ec3df76ffe4462cb8ce045
UpcomingWhale <javascriptcodingguru@gmail.com>
2023-02-23 10:49:42 -0500

    update UserMessages.jsx
    now filters messages based off of active posts

--- src/components/UserMessages.jsx
+++ src/components/UserMessages.jsx
@@ -1,28 +1,48 @@
 import React, {useState, useEffect} from 'react'
-import {displayMessages} from '../apiAdapters/index'
+import {displayMessages, getAllPosts} from '../apiAdapters/index'
 
 
 export default function UserMessages()
 {
     let [messages, setMessages] = useState([]);
-    console.log(messages)
 
-    async function getMessages()
+    async function getMyPosts()
     {
         try
         {
-            let result = await displayMessages();
-            console.log(result);
-            setMessages(result);
+            let result = await getAllPosts();
+            let messages = await displayMessages();
+            let holder = [];
+            let messageHolder = [];
+            for(let i = 0; i < result.length; i++)
+            {
+                console.log(result[i].isAuthor)
+                if(result[i].isAuthor)
+                {
+                    holder.push(result[i]);
+                }
+            }
+            for(let i = 0; i < messages.length; i++)
+            {
+                for(let j = 0; j < holder.length; j++)
+                {
+                    if(messages[i].post._id === holder[j]._id)
+                    {
+                        messageHolder.push(messages[i]);
+                    }
+                }
+            }
+            setMessages(messageHolder);
         }
         catch(e)
         {
             console.error(e);
         }
     }
+
     useEffect(()=>
     {
-        getMessages();
+        getMyPosts();
     }, [])
     return(
         <div>
@@ -31,7 +51,8 @@ export default function UserMessages()
         {
             return(
                 <div key={idx}>
-                    <h3>{message.post.title}</h3>
+                <h3>{message.post.title}</h3>
+                <p>{message.content}</p>
                 </div>
             )
         }):null
